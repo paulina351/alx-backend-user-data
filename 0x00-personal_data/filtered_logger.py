8c00da45e8fd3c79c06dd303c7c572f0a23d66d1
Paulina Udeh <udehpaulina93@gmail.com>
2024-04-12 04:41:30 +0000

    filtered logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -3,27 +3,34 @@
 """
 
 import re
-from typing import List
 import logging
+import os
+import mysql.connector
+from typing import List
 
 PIL_FIELDS = ('name', 'password', 'phone', 'ssn', 'email')
 
 
-def filter_datum(fields: List[str], redaction: str,
-                 message: str, seperator: str) -> str:
-    """log message"""
-    for field in fields:
-        message = re.sub(f'{field}=.*?{seperator}',
-                         f'{field}={redaction}{seperator}', message)
-    return message
+def get_db() -> mysql.connector.connection.MySQLConnection:
+    """connection to mysql"""
+    user = os.environ.get("PERSONAL_DATA_DB_USERNAME", "root")
+    password = os.environ.get("PERSONAL_DATA_DB_PASSWORD", "")
+    host = os.environ.get("PERSONAL_DATA_DB_HOST", "localhost")
+    db_name = os.environ.get("PERSONAL_DATA_DB_NAME")
+    myDB = mysql.connector.connect(
+            user=user,
+            password=password,
+            host=host,
+            database=db_name
+    )
+    return myDB
 
+class RedactingFormatter(logging.Formatter):
+    """A class implementation"""
 
-class RdactingFormatter(logging.Formatter):
-    """RedactingFormatter"""
-
-    REDACTION = "***"
+    REDACTION = "****"
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
-    SEPERATOR = ";"
+    SEPERATOR =";"
 
     def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
@@ -35,6 +42,13 @@ class RdactingFormatter(logging.Formatter):
                             super(RedactingFormatter, self).format(record),
                             self.SEPERATOR)
 
+        def filter_datum(fields: List[str], redaction: str,
+                         message: str, eperator: str) -> str:
+            """return log"""
+            for field in fields:
+                message = re.sub(rf'{field}=(.*?){seperator}',
+                                 f'{field}={redaction}{seperator}', message)
+            return message
 
 def get_logger() -> logging.Logger:
     """logging object"""
@@ -46,3 +60,21 @@ def get_logger() -> logging.Logger:
     logger.addHandler(stream_handler)
 
     return logger
+
+def main() -> None:
+    """Function"""
+    db = get_db()
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM users;")
+    logger = get_logger()
+    for row in cursor:
+        message = ""
+        for a in range(len(row)):
+            message += f"{PIL_FIELDS[a]}={row[a]};"
+        logger.info(message)
+    cursor.close()
+    db.close()
+
+
+if __name__ == "__main__":
+    main()
